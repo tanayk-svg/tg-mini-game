b845e33ea37e41b2ad4a3de53e1fcea674c3314a
Tanay <tanay0364@gmail.com>
2024-10-09 20:33:30 +0530

    inital-Commit-2

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Rock Paper Scissor</h1>
+      <h1>Tanay</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
