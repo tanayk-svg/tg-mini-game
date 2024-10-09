a7dbf572b12349654689af57012f92f8f9cce292
Tanay <tanay0364@gmail.com>
2024-10-09 20:37:18 +0530

    without telegram js

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Rock Paper Scissor</h1>
+      <h1>Rock Paper Scissors</h1>
       <h1>Tanay</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
